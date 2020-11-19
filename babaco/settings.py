7dd86a1a7041feff91522005a1d7a3449035abdc
Vignesh1357 <vinayv1357@gmail.com>
2020-11-19 15:38:19 +0530

    Updated templates

--- babaco/settings.py
+++ babaco/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'store.apps.StoreConfig',    #our store app has been linked!
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'babaco.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Babaco',
+        'USER' : 'postgres',
+        'PASSWORD' : 'minchuv',
+        'HOST' : 'localhost'
     }
 }
 
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(req):
-    return HttpResponse("<H1> Hello, World!</H1>")
+    return render(req, 'store/home.html',{'title':'Babaco-Home'})
