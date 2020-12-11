814fc6783dd4d87f648b1cb9a6a0aa61fac2daf5
Uvaismon <uvaisullas@gmail.com>
2020-12-11 19:49:25 +0530

    Added side navigation bar for store user. Implemented Add products and orders button inside side navigation bar of store user.

--- store/urls.py
+++ store/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('addproduct/', views.store_addproduct_view, name='addproduct.store'),
     path('profile/', views.profile, name='profile.store'),
     path('profile/logout/', views.logout, name='logout.store'),
-    path('orders/',views.orders_view, name='orders.store'),
+    path('orders/', views.orders_view, name='orders.store'),
 ]
--- store/views.py
+++ store/views.py
@@ -12,8 +12,8 @@ from shop.tests import logging
 def home(req):
     if not req.session.get('store_id'):
         return redirect(store_login_view)
-    store = Store.objects.get(pk=req.session.get('store_id'))    # db trans
-    products = Product.objects.filter(store_id=store)   # db trans
+    store = Store.objects.get(pk=req.session.get('store_id'))  # db trans
+    products = Product.objects.filter(store_id=store)  # db trans
     context = {'title': 'home', 'user': 'store', 'store_name': req.session.get('store_name'),
                'products': products, 'media_url': MEDIA_URL}
     return render(req, 'store/home.html', context)
@@ -36,7 +36,7 @@ def store_addproduct_view(req):
     # Checking if its a POST request and handle it.
     if req.method == 'POST':
         post = req.POST.copy()
-        post['store_id'] = Store.objects.get(pk=store_id)                                                      # dbtrans
+        post['store_id'] = Store.objects.get(pk=store_id)  # dbtrans
         form = AddproductForm(post, req.FILES)
         if form.is_valid():
             form.save()
@@ -56,36 +56,33 @@ def profile(req):
     else:
         return redirect('store/login/')
 
+
 def orders_view(req):
     if req.session.get('store_id'):
-        store_id = Store.objects.get(pk=req.session.get('store_id'))    #dbtrans
+        store_id = Store.objects.get(pk=req.session.get('store_id'))  # dbtrans
         products = Order.objects.all()
         storeorder_id = []
-        #storeprod_id = []
 
         for product in products:
             order_id = product.order_id
             prod_id = product.prod_id
-            if prod_id.store_id == store_id :
-                order_details = Order.objects.get(order_id=order_id)       #dbtrans
+            if prod_id.store_id == store_id:
+                order_details = Order.objects.get(order_id=order_id)  # dbtrans
                 storeorder_id.append(order_details)
-
-                #product_name = Product.objects.get(prod_id=prod_id.pk)
-                #storeprod_id.append(product_name)
-            else:
-                pass
-        try :
+        try:
             print(storeorder_id[0])
             paginator = Paginator(storeorder_id, 2)
             page = req.GET.get('page')
             storeorder_id = paginator.get_page(page)
-            context = {'order_details' : storeorder_id }
-            return render(req,'store/storeorders.html', context)
+            context = {'order_details': storeorder_id}
+            return render(req, 'store/storeorders.html', context)
+
         except:
-            context = {'order_details': 'zero' }
+            context = {'order_details': 'zero'}
             return render(req, 'store/storeorders.html', context)
     else:
         return redirect('store/login/')
+
 
 def logout(req):
     req.session.flush()
