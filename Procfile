4ca34ca76b58617d4fae60e62dc293c2ce90fc49
Uvaismon <uvaisullas@gmail.com>
2020-12-27 16:41:58 +0530

    Heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn babaco.py
+web: gunicorn babaco.wsgi
--- babaco/settings.py
+++ babaco/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = 'static'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if not DEBUG:
     pass
 
