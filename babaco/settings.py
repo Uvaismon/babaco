079a98e5fc0adf17c66f7772525300748534ed61
Uvaismon <uvaisullas@gmail.com>
2020-12-28 12:38:14 +0530

    Set up firebase storages for image uploads

--- babaco/settings.py
+++ babaco/settings.py
@@ -131,5 +131,23 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+if not DEBUG:
+
+    firebaseCred = {
+        'type': 'service_account',
+        'project_id': 'babaco-e61f8',
+        'private_key_id': os.environ.get('firebase_private_key_id'),
+        'private_key': os.environ.get('firebase_private_key'),
+        'client_email': 'firebase-adminsdk-3sk3q@babaco-e61f8.iam.gserviceaccount.com',
+        'client_id': '102367086375451386626',
+        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+        'token_uri': 'https://oauth2.googleapis.com/token',
+        'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+        'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3sk3q%40babaco-e61f8.iam.gserviceaccount.com'
+    }
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+    GS_BUCKET_NAME = 'babaco-e61f8.appspot.com'
+    GS_CREDENTIALS = service_account.Credentials.from_service_account_info(firebaseCred)
 
 django_heroku.settings(locals())
